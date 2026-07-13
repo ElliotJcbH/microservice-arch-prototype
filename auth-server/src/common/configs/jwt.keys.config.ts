a14d6323a61b1ea33cbd09e505253e18c588a7bc
ElliotJcbH <elliot.jacob09@gmail.com>
2026-07-13 18:57:25 +0800

    updates keys config service to have a general path with keysDirectory variable

--- auth-server/src/common/configs/jwt.keys.config.ts
+++ auth-server/src/common/configs/jwt.keys.config.ts
@@ -1,16 +1,20 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-const keysDirectory = path.join(process.cwd(), 'keys', 'v1');
+const keysDirectory = path.join(process.cwd(), 'keys'); // i.e. /auth-server/keys
 
 export const KEY_STORE = {
     currentKeyId: 'v1',
-    refresh_token_timer_ms:  1000 * 60 * 60 * 24 * 30, // 30 days
+    refresh_token_timer_ms: 1000 * 60 * 60 * 24 * 30, // 30 days
     access_token_timer_ms: 1000 * 10, // 10 seconds
     keys: {
-        'v1': {
-            publicKey: fs.readFileSync(path.join(keysDirectory, 'public.pem')),
-            privateKey: fs.readFileSync(path.join(keysDirectory, 'private.pem')),
-        }
-    }
+        v1: {
+            publicKey: fs.readFileSync(
+                path.join(keysDirectory, 'v1', 'public.pem'),
+            ),
+            privateKey: fs.readFileSync(
+                path.join(keysDirectory, 'v1', 'private.pem'),
+            ),
+        },
+    },
 };
